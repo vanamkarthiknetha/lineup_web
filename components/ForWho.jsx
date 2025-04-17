ba6bbf783e9dee857296da37af3a438270d58858
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-04-17 21:10:51 +0530

    fixed screens

--- components/ForWho.jsx
+++ components/ForWho.jsx
@@ -13,6 +13,40 @@ export default function ForWho() {
     "/who/HelloWorld.png",
     "/who/Sound.png",
   ];
+  const descriptions = [
+    
+    {
+      screen: 1,
+      heading: "Members",
+      tagline: "Attendees & promoters",
+      desc: "Looking for events this month,  create your next gig, or throwing a  house party? LineUp lets you grab entries for the best nights out, set up your own events effortlessly, and earn cash by sharing your favorite gigs. ",
+    },
+    {
+      screen: 2,
+      heading: "Labels",
+      tagline: "Music labels, event labels or brands",
+      desc: "Need to push your music, discover fresh talent, land gigs, or level up? LineUp puts you in front of the city's hottest venues and event organizers. Showcase your profile, get booked, connect with DJs and collectives, or launch your own events. Boost your reach, stack your revenue, and own the night. ",
+    },
+    {
+      screen: 3,
+      heading: "Venues",
+      tagline:
+        "nightclub, bar, restaurant, or any venue looking to host events.",
+      desc: "Nightclub, bar, restaurant, boutique—or any spot? LineUp got you . List your venue, get discovered by the city's greatest event organizers, or book DJs, collectives, labels to pack your space. Easily set up entries, sell out nights, and own your city.",
+    },
+    {
+      screen: 4,
+      heading: "DJs",
+      tagline: "Speak for itself",
+      desc: "You want to increase your numbers of bookings and get noticed by the hottest event organizer around the city. Or maybe throw your own gigs? LineUp is the platform you need. ",
+    },
+    {
+      screen: 5,
+      heading: "Collectives",
+      tagline: "Collectives of djs or event organizers",
+      desc: "You have a collective and you are willing to find spots to organize your next event? or want to find other performers to collaborate with? Maybe you want to get booked for exclusive event. LineUp is the place for you.",
+    },
+  ];
 
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true, amount: 0.3 });
@@ -25,18 +59,17 @@ export default function ForWho() {
 
       {/* Main container with hidden scrollbar */}
       <motion.div
-      ref={ref}
-      initial={{ y: 80, opacity: 0 }}
-      animate={isInView ? { y: 0, opacity: 1 } : {}}
-      transition={{ duration: 0.7, ease: "easeOut" }}
-      className="relative w-full h-[60vh] sm:h-[75vh] overflow-x-auto overflow-y-hidden hide-scrollbar">
+        ref={ref}
+        initial={{ y: 80, opacity: 0 }}
+        animate={isInView ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+        className="relative w-full h-[60vh] sm:h-[75vh] overflow-x-auto overflow-y-hidden hide-scrollbar"
+      >
         {/* Flex container for centering content */}
         <div className="flex pl-24 sm:pl-0 xl:justify-center gap-4 whitespace-nowrap">
           {mobileScreens.map((screen, index) => {
-
             return (
               <div
-                
                 key={index}
                 className={`relative transition-all duration-500 ease-in-out ${
                   hoveredIndex === index ? "z-10" : "z-1"
@@ -67,13 +100,16 @@ export default function ForWho() {
         </div>
       </motion.div>
 
-      <motion.div 
-      ref={textRef}
-      initial={{ y: 80, opacity: 0 }}
-      animate={textIsInView ? { y: 0, opacity: 1 } : {}}
-      transition={{ duration: 0.7, ease: "easeOut" }}
-      className="text-center">
-        <h2 className="text-[50px] font-semibold">Venues</h2>
+      <motion.div
+        ref={textRef}
+        initial={{ y: 80, opacity: 0 }}
+        animate={textIsInView ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 0.7, ease: "easeOut" }}
+        className="text-center"
+      >
+        {hoveredIndex === null ? (
+          <>
+            <h2 className="text-[50px] font-semibold">Venues</h2>
         <p className="text-desc text-[15px]">
           nightclub, bar, restaurant, or any venue looking to host events.
         </p>
@@ -83,6 +119,21 @@ export default function ForWho() {
           organizers, or book DJs, collectives, labels to pack your space.
           Easily set up entries, sell out nights, and own your city.
         </p>
+          </>
+        ) : (
+          <>
+            <h2 className="text-[50px] font-semibold">
+              {descriptions[hoveredIndex]?.heading}
+            </h2>
+            <p className="text-desc text-[15px]">
+              {descriptions[hoveredIndex]?.tagline}
+            </p>
+            <p className="text-[20px] max-w-[95%] sm:max-w-[55%] mx-auto mt-10">
+              {descriptions[hoveredIndex]?.desc}
+            </p>
+          </>
+        )}
+        
       </motion.div>
     </div>
   );
