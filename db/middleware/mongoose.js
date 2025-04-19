6df83ad6583ea077c25d5d9f58d5a436c74f1367
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-04-19 11:43:34 +0530

    clg

--- db/middleware/mongoose.js
+++ db/middleware/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDb=async ()=>{
     if(mongoose.connections[0].readyState) return
-    // console.log("connecting to db:",process.env.MONGODB_URI)
+    console.log("connecting to db:",process.env.MONGODB_URI)
     await mongoose.connect(process.env.MONGODB_URI);
 }
 
