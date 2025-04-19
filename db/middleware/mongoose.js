d17d50a5e10987013ad6c4b559cd31ae680b207a
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-04-19 11:29:39 +0530

    update db config

--- db/middleware/mongoose.js
+++ db/middleware/mongoose.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const connectToDb=async ()=>{
     if(mongoose.connections[0].readyState) return
     // console.log("connecting to db:",process.env.MONGODB_URI)
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
 }
 
 export default connectToDb
