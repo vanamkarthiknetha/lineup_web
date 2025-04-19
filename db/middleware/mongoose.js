42e527fef789679055c7438fc8982a9c21124560
vanamkarthiknetha <vanamkarthiknetha@gmail.com>
2025-04-19 11:42:42 +0530

    remove db config options

--- db/middleware/mongoose.js
+++ db/middleware/mongoose.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 const connectToDb=async ()=>{
     if(mongoose.connections[0].readyState) return
     // console.log("connecting to db:",process.env.MONGODB_URI)
-    await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+    await mongoose.connect(process.env.MONGODB_URI);
 }
 
 export default connectToDb
